b12b6721478468ae19036c825925cb0470659900
aurovindosahu <aurovindosahu@BLR-DT-101067.flexera.com>
2019-04-29 09:36:31 +0530

    commit changes

---
+++ spring-cloud-user-login/src/main/java/com/user/management/microservices/springclouduserlogin/SpringCloudUserLoginApplication.java
@@ -0,0 +1,13 @@
+package com.user.management.microservices.springclouduserlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudUserLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudUserLoginApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-user-login/src/test/java/com/user/management/microservices/springclouduserlogin/SpringCloudUserLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.user.management.microservices.springclouduserlogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudUserLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
