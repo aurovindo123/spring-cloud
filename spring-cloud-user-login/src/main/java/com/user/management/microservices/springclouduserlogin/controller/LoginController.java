4249e1a408d750af98e2a20e9b7e9999b33b270f
aurovindosahu <aurovindosahu@BLR-DT-101067.flexera.com>
2019-04-29 09:49:55 +0530

    Login controller

---
+++ spring-cloud-user-login/src/main/java/com/user/management/microservices/springclouduserlogin/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.user.management.microservices.springclouduserlogin.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+		
+	@PostMapping("/login")
+	public void login(@RequestBody String userName, @RequestBody String password) {
+		
+	}
+}
