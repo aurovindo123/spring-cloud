2d68d7cd3b29907ea5f87fd74fbdb32e60f871a6
Aurovindo <tech.aurovindo13@gmail.com>
2019-05-01 07:21:03 +0530

    cpring cloud user registration project created.

---
+++ spring-cloud-user-registration/src/main/java/com/user/management/microservices/springclouduserregistration/SpringCloudUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.user.management.microservices.springclouduserregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudUserRegistrationApplication.class, args);
+	}
+
+}
